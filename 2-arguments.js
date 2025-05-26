df462fae75fa641de7f8cae8dd16cc38bf28d5e3
Fatai <kenkufatai@gmail.com>
2025-05-26 16:10:41 +0100

    Update 2-arguments.js with argument count handling

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,22 +1,9 @@
-if (process.argv.length <= 2) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
 const argsCount = process.argv.length - 2;
 
 if (argsCount === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argsCount === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
-}const argsCount = process.argv.length - 2;
-
-if (argsCount === 0) {
-  console.log("No argument");
-} else if (argsCount === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
