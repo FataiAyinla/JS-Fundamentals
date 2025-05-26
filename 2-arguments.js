b796e944bbc7ba7f79d634eeb2b92ab203abfaf4
Fatai <kenkufatai@gmail.com>
2025-05-26 16:05:01 +0100

    2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,3 +3,20 @@ if (process.argv.length <= 2) {
 } else {
   console.log(process.argv[2]);
 }
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
+  console.log("No argument");
+} else if (argsCount === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
+  console.log("No argument");
+} else if (argsCount === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
